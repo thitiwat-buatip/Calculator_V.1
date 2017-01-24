b38a5ebe455ede0676e2ce29e3fd84b8eae3f51d
thitiwat-buatip <subin2538@gmail.com>
2017-01-24 22:37:52 +0700

    upload file

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,7 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var numberOnscreen: Double = 0
+    var previousNumber: Double = 0
+    var performingMath = false
+    var operation = 0
+    @IBOutlet weak var label: UILabel!
+    @IBAction func numBers(_ sender: UIButton)
+    {
+        
+        if performingMath == true
+        {
+            label.text = String(sender.tag-1)
+            numberOnscreen = Double(label.text!)!
+            performingMath = false
+            
+        }
+        else
+        {
+            label.text = label.text! + String(sender.tag-1)
+            numberOnscreen = Double(label.text!)!
+        }
+        
+    }
+    
+    @IBAction func Operator(_ sender: UIButton)
+    {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16
+        {
+            previousNumber = Double(label.text!)!
+            
+            if sender.tag == 12 // Divide
+            {
+                label.text = "/"
+            }
+            else if sender.tag == 13 //Multiply
+            {
+                label.text = "*"
+            }
+            else if sender.tag == 14 //Minus
+            {
+                label.text = "-"
+            }
+            else if sender.tag == 15 //Plus
+            {
+                label.text = "+"
+            }
+            performingMath = true
+            operation = sender.tag
+        }
+        else if sender.tag == 16
+        {
+            if operation == 12
+            {
+                label.text = String(previousNumber / numberOnscreen)
+            }
+            else if operation == 13
+            {
+                label.text = String(previousNumber * numberOnscreen)
+            }
+            else if operation == 14
+            {
+                label.text = String(previousNumber - numberOnscreen)
+            }
+            else if operation == 15
+            {
+                label.text = String(previousNumber + numberOnscreen)
+            }
+            performingMath = true	
+            
+        }
+        else if sender.tag == 11
+        {
+            label.text = ""
+            previousNumber = 0
+            numberOnscreen = 0
+            operation = 0
+        }
+        
+    }
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
